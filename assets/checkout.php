ad6d266fb6cbf24f9dcc745ef1f7c7bbc3971faa
bao-ne <letranbao10@gmail.com>
2023-01-05 13:15:26 +0700

    cart.php

---
+++ assets/checkout.php
@@ -0,0 +1,15 @@
+<?php 
+    require_once 'view/header.php';
+
+?>
+
+    
+    <div class="container-fluid">
+        <div class="row bg-secondary py-1 px-xl-5"><?php require_once 'cpn/checkoutcom.php'; ?></div>
+    </div>
+   
+    
+
+<?php 
+    require_once 'view/footer.php';
+?>
